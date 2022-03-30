246ca48aaaeb30a65de278931792ec46b0a7b4c2
Stanley <zylizj@tsmc.com>
2022-03-30 16:59:51 +0800

    [Feature]update ps1 file content

--- chocoInstall.py
+++ chocoInstall.py
@@ -14,6 +14,35 @@ def list_dir():
     
     return dirs
 
+def update_dir_file(dir, oldStr, newStr):
+    fileData = ""
+
+    file = copylibDir + "/" + dir +"/tools/chocolateyinstall.ps1"
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if oldStr in line:
+                line = line.replace(oldStr, newStr)
+            fileData += line
+    
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(fileData)
+
+def update_url_to_filePath(dir):
+    fileData = ""
+
+    file = copylibDir + "/" + dir +"/tools/chocolateyinstall.ps1"
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if 'http' in line:
+                URL = line[line.find('\''):line.rfind('\'')+1]
+                fileName = line[line.rfind('/')+1:line.rfind('\'')]
+                fileName = fileName.replace('%20', '_')
+                line = line.replace(URL, "\""+copylibDir + "/" + dir + "/tools/" + fileName+"\"")           
+            fileData += line
+    
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(fileData)
+    
 def download_dir_file(dir):
     f = open(path + "/" + dir +"/tools/chocolateyinstall.ps1", "r")
     lines = f.readlines()
@@ -32,18 +61,22 @@ def download_dir_file(dir):
             
     f.close
 
-def copyDir():
+def copy_dir():
     global copylibDir
     currentDir = str(pathlib.Path().resolve())
     copylibDir = currentDir + "_copylib"
 
     shutil.copytree(path, copylibDir)
 
-copyDir()
+# # 複製 programData 到 當前目錄
+copy_dir()
 
+# 列出下載的全部資料夾
 dirs = list_dir()
 
+#下載資料夾內ps1的url
 for dir in dirs:
     download_dir_file(dir)
-
-download_dir_file('lens')
+    
+    update_dir_file(dir, "url", "file")
+    update_url_to_filePath(dir)
